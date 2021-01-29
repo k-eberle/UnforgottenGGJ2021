ed7cdf5144555ac6710e90fa93eb8815b1bc715a
nburkard <nburkard@rhrk.uni-kl.de>
2021-01-29 19:14:50 +0100

    added Player movement

---
+++ UnforgottenUnity/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : PhysicsObject
+{
+    public float maxSpeed = 7f;
+    public float jumpTakeOffSpeed = 7;
+
+    public Vector2 move;
+
+    private SpriteRenderer spriteRenderer;
+    private Animator animator;
+
+    private void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
+    }
+
+    protected override void ComputeVelocity()
+    {
+        base.ComputeVelocity();
+        Vector2 movement = Vector2.zero;
+        move.x = Input.GetAxis("Horizontal");
+        if (Input.GetButtonDown("Jump") && grounded)
+        {
+            velocity.y = jumpTakeOffSpeed;
+        }
+        else if (Input.GetButtonUp("Jump"))
+        {
+            if(velocity.y > 0)
+            {
+                velocity.y = velocity.y * 0.5f;
+            }
+        }
+
+        targetVelocity = move * maxSpeed;
+    }
+
+    protected override void FlipSprite()
+    {
+        bool flipSprite = (spriteRenderer.flipX ? (move.x < -0.01f) : (move.x > 0.01f));
+        if (flipSprite)
+        {
+            spriteRenderer.flipX = !spriteRenderer.flipX;
+        }
+    }
+
+}
