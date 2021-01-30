7b857c41ffa623c2ce58c954bed744ca2a0c9926
Kevin Eberle <keberle@rhrk.uni-kl.de>
2021-01-30 01:18:55 +0100

    Imported many sprites and other stuff.

---
+++ UnforgottenUnity/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Level1");
+    }
+
+    public void ExitGame()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+//#elif UNITY_WEBPLAYER
+//         Application.OpenURL(webplayerQuitURL);
+//#else
+         Application.Quit();
+#endif
+    }
+}
