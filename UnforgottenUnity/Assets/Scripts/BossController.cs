0e519ae9bc505dc6c1adbb866cb6a4dd43984151
Kevin Eberle <keberle@rhrk.uni-kl.de>
2021-01-30 13:24:23 +0100

    More boss stuff and tile set for boss level.

--- UnforgottenUnity/Assets/Scripts/BossController.cs
+++ UnforgottenUnity/Assets/Scripts/BossController.cs
@@ -1,12 +1,16 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 
 public class BossController : MonoBehaviour
 {
     private enum BossState
     {
-        Start,
         Spawning,
-        Idle
+        Idle,
+        WaitingForAttack,
+        PuppetAttack,
+        SmashAttack,
+        ClapAttack
     }
 
     private BossState state;
@@ -14,23 +18,34 @@ public class BossController : MonoBehaviour
 
     private void Start()
     {
-        state = BossState.Start;
+        state = BossState.Spawning;
     }
     
     private void Update()
     {
         switch (state)
         {
-            case BossState.Start:
-                AnimateSpawn();
+            case BossState.Spawning:
+                break;
+            case BossState.Idle:
+                StartCoroutine(ComputeNextAttack());
                 break;
             default:
                 break;
         }
     }
 
-    private void AnimateSpawn()
+    private IEnumerator ComputeNextAttack()
     {
+        state = BossState.WaitingForAttack;
+        Debug.Log("Waiting.");
+        yield return new WaitForSeconds(2.0f);
+        Debug.Log("Waited.");
+        state = BossState.Idle;
+    }
 
+    public void GoIdle()
+    {
+        state = BossState.Idle;
     }
 }
