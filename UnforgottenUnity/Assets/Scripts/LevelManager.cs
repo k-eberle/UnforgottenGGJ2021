ee9a9d9ba2d347187e5a0a4f2e0ca32d150ad182
Feowinn <60522416+Feowinn@users.noreply.github.com>
2021-01-30 18:21:04 +0100

    added foreground and background, new tiles

---
+++ UnforgottenUnity/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+
+    public int nextScene;
+
+    private Canvas canvas;
+    private TMP_Text text;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnEnable()
+    {
+        //canvas = (Canvas)GameObject.FindObjectOfType(typeof(Canvas));
+        //text = canvas.GetComponentInChildren<TMP_Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ShowNextText()
+    {
+        //canvas.enabled = true;
+
+    }
+
+
+}
