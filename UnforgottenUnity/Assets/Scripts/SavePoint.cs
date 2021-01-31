c25408a050928ccef3f129b817350cf2252c24f6
Kevin Eberle <keberle@rhrk.uni-kl.de>
2021-01-31 13:05:47 +0100

    Added sprite to savepoint.

--- UnforgottenUnity/Assets/Scripts/SavePoint.cs
+++ UnforgottenUnity/Assets/Scripts/SavePoint.cs
@@ -1,25 +1,32 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class SavePoint : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public SpriteRenderer spriteRenderer;
+    public Sprite unactivatedSprite;
+    public Sprite activatedSprite;
+    public GameObject lightSource;
+
+    private bool isActivated;
+
+
+    private void Awake()
     {
-        
+        isActivated = false;
+        spriteRenderer.sprite = unactivatedSprite;
+        lightSource.SetActive(false);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     public void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
+            isActivated = true;
+            spriteRenderer.sprite = activatedSprite;
+            lightSource.SetActive(true);
+
+
             PlayerPrefs.SetInt("LoadSave", 1);
             PlayerPrefs.SetFloat("SaveX", transform.position.x);
             PlayerPrefs.SetFloat("SaveY", transform.position.y);
