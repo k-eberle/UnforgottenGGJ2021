b72d82b7285d14d7c52bc0221ea1991de831f114
Feowinn <60522416+Feowinn@users.noreply.github.com>
2021-01-31 11:22:23 +0100

    skills can now be unlocked!

---
+++ UnforgottenUnity/Assets/Scripts/UnlockDoubleJump.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnlockDoubleJump : MonoBehaviour
+{
+    public string description = "You unlocked the DoubleJump!\nPress Space in midair to Jump again";
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            collision.gameObject.GetComponent<PlayerController>().unlockedDoubleJump = true;
+            GameObject.FindObjectOfType<LevelManager>().ShowCustomTextNext(description);
+        }
+    }
+}
